7780c1b09730bf52e917f28beafe408d627bc822
bhsbhs235 <bhsbhs235@naver.com>
2026-03-12 22:55:46 +0900

    first commit

---
+++ src/main/java/com/example/zipsa/ZipSaApplication.java
@@ -0,0 +1,13 @@
+package com.example.zipsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZipSaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZipSaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/zipsa/ZipSaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zipsa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZipSaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
